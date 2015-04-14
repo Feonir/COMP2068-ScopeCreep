b2c26f3e5fbe7c59d018f1f93a25ec7b84e14f45
Feonir <chemicle_nova@hotmail.com>
2015-04-13 22:02:13 -0400

    Uploading Project As Is

---
+++ COMP2068-ScopeCreep/Scripts/states/gameover.ts
@@ -0,0 +1,63 @@
+﻿/// <reference path="../constants.ts" />
+/// <reference path="../objects/gameobject.ts" />
+/// <reference path="../objects/button.ts" />
+/// <reference path="../objects/label.ts" />
+/// <reference path="../objects/scoreboard.ts" />
+
+
+module states {
+    // PLAY STATE
+    export class GameOver {
+        // INSTANCE VARIABLES ++++++++++++++++++++++++++++++++++++++++++++++
+        public game: createjs.Container;
+        public grass: objects.Grass;
+        public tryAgainButton: objects.Button;
+
+        // CONSTRUCTOR ++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        constructor() {
+            // Instantiate Game Container
+            this.game = new createjs.Container();
+
+            // Add ocean to game
+            this.grass = new objects.Grass();
+            this.game.addChild(this.grass);
+
+            var gameOverLabel: objects.Label = new objects.Label("You Died", constants.SCREEN_CENTER_WIDTH, 100);
+            gameOverLabel.font = "60px Consolas";
+            gameOverLabel.regX = gameOverLabel.getMeasuredWidth() * 0.5;
+            gameOverLabel.regY = gameOverLabel.getMeasuredHeight() * 0.5;
+            this.game.addChild(gameOverLabel);
+
+            var finalScoreLabel: objects.Label = new objects.Label("FINAL SCORE: " + finalScore, constants.SCREEN_CENTER_WIDTH, 200);
+            this.game.addChild(finalScoreLabel);
+
+            var highScoreLabel: objects.Label = new objects.Label("HIGH SCORE: " + highScore, constants.SCREEN_CENTER_WIDTH, 300);
+            this.game.addChild(highScoreLabel);
+
+            this.tryAgainButton = new objects.Button("assets/images/blueButton.jpg", constants.SCREEN_CENTER_WIDTH, 400);
+            this.game.addChild(this.tryAgainButton);
+            this.tryAgainButton.on("click", this.tryAgainButtonClicked, this);
+
+
+            stage.addChild(this.game);
+        } // constructor end
+
+
+        // PUBLIC METHODS ++++++++++++++++++++++++++++++++++++++++++++++++++++
+        tryAgainButtonClicked() {
+            this.game.removeAllChildren();
+            stage.removeChild(this.game);
+            currentState = constants.PLAY_STATE;
+            stateChanged = true;
+        }
+
+        // UPDATE METHOD
+        public update() {
+
+            
+
+        } // update method end
+
+
+    }
+}  
