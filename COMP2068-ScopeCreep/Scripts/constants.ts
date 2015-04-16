9c78b2f8bb275c510576bb61f24c35b4f1d3f31e
Feonir <chemicle_nova@hotmail.com>
2015-04-15 20:50:56 -0400

    More minor tweaks.

--- COMP2068-ScopeCreep/Scripts/constants.ts
+++ COMP2068-ScopeCreep/Scripts/constants.ts
@@ -2,7 +2,7 @@
     // Font Constants
     export var FONT_SIZE: string = "40px";
     export var FONT_FAMILY: string = "Consolas";
-    export var FONT_COLOUR: string = "#FFFF00";
+    export var FONT_COLOUR: string = "#FF0000";
 
     // Stage Constants
     export var SCREEN_WIDTH: number = 640;
@@ -14,7 +14,7 @@
     // Game Constants
     export var PLAYER_LIVES: number = 5;
     export var ZOM_NUM: number = 5;
-
+    export var ZOM_SPEED: number = 1;
     // States Constants
     export var MENU_STATE: number = 0;
     export var PLAY_STATE: number = 1;
